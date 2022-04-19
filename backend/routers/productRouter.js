a9830ccfc94ca948c1e8a0aa23b33982985380d4
Siddharth B <spetnazsid@gmail.com>
2022-04-20 01:08:50 +0530

    updated backend

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -9,11 +9,11 @@ productRouter.get(
   expressAsyncHandler(async (req, res) => {
     const searchKeyword = req.query.searchKeyword
       ? {
-          name: {
-            $regex: req.query.searchKeyword,
-            $options: "i",
-          },
-        }
+        name: {
+          $regex: req.query.searchKeyword,
+          $options: "i",
+        },
+      }
       : {};
     const products = await Product.find({ ...searchKeyword });
     res.send(products);
